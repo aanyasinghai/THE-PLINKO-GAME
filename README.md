1f47c9f7001292f9357ebd2b207d5a4fc0ece841
aanyasinghai <64785041+aanyasinghai@users.noreply.github.com>
2020-08-23 10:52:24 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# THE-PLINKO-GAME
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,64 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var particle = [];
+var plinko = [];
+var division = [];
+
+var ground;
+var divisionHeight = 200;
+
+function setup() {
+  createCanvas(480,800);
+  engine = Engine.create();
+   world = engine.world;
+
+   ground = new Ground(240,750,480,50);
+
+   for(var i =5;i<480;i=i+70){
+     division.push(new Division(i,height- divisionHeight/2,10,divisionHeight));
+   } 
+
+   for(var i = 26; i<480; i = i+30){
+     plinko.push(new Plinko(i,100));
+   }
+
+   for(var i=10;i<480; i=i+30){
+     plinko.push(new Plinko(i,150));
+   }
+
+   for(var i = 25;i<480; i = i+30){
+     plinko.push(new Plinko(i,200));
+   }
+
+   for(var i= 10;i<480;i=i+30){
+     plinko.push(new Plinko(i,250));
+   }
+}
+                                                                                                                                                                                                            
+function draw() {
+  background(0);  
+  
+  Engine.update(engine);
+
+  if(frameCount % 30 === 0){
+    particle.push(new Particle(random(width/2-15,width/2+15)));
+  }
+
+  ground.display();
+
+  for (var k = 0;k<division.length;k++){
+    division[k].display();
+  }
+
+  for(var j=0;j< plinko.length;j++){
+    plinko[j].display();
+  }
+
+  for(var j =0;j< particle.length;j++){
+    particle[j].display();
+  }
+
+  drawSprites();
+}
